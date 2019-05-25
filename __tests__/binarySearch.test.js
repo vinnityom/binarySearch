40f744cf90c46dd08a53db3892d616cb57b78c38
artem <sukharevartyom@yandex.ru>
2019-05-25 20:10:50 +0300

    add tests

---
+++ __tests__/binarySearch.test.js
@@ -0,0 +1,16 @@
+import binarySearch from '../src/binarySearch';
+
+describe('should get index right', () => {
+  const numbers = [1, 2, 3, 5, 6, 8, 9, 10, 11, 12];
+
+  test.each([
+    [3, 2],
+    [4, null],
+    [9, 6],
+    [14, null],
+  ])('index of %i should be %p',
+    (numberToSearch, expected) => {
+      expect(binarySearch(numbers, numberToSearch)).toBe(expected);
+    },
+  );
+});
