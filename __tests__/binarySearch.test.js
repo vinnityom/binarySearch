39ce1bdce3b9d4fb1d89376d4fed4840dc6c6db2
artem <sukharevartyom@yandex.ru>
2019-05-25 20:52:35 +0300

    add test for repeating numbers

--- __tests__/binarySearch.test.js
+++ __tests__/binarySearch.test.js
@@ -1,12 +1,13 @@
 import binarySearch from '../src/binarySearch';
 
 describe('should get index right', () => {
-  const numbers = [1, 2, 3, 5, 6, 8, 9, 10, 11, 12];
+  const numbers = [1, 2, 3, 5, 6, 8, 9, 10, 11, 12, 12];
 
   test.each([
     [3, 2],
     [4, null],
     [9, 6],
+    [12, 9],
     [14, null],
   ])('index of %i should be %p',
     (numberToSearch, expected) => {
